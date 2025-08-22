b44ecdea9f7d7497c765cb2b4e87b5a5a7f48b2e
Ripanul Alam Ridoy <ripanulalam8@gmail.com>
2025-08-22 12:41:27 +0600

    result added

--- src/Pages/ClientResult.jsx
+++ src/Pages/ClientResult.jsx
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet-async";
 import { Loading } from "../components/Shared/Loading";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { motion, AnimatePresence } from "framer-motion";
 const ClientResult = () => {
   const [examName, setExamName] = useState("1st-Semester");
   const [session, setSession] = useState(new Date().getFullYear().toString());
@@ -16,7 +17,7 @@ const ClientResult = () => {
     isLoading,
     refetch,
   } = useQuery({
-    enabled, 
+    enabled,
     queryKey: ["result", session, className, classRoll, examName],
     queryFn: async () => {
       const { data } = await axios(
@@ -34,19 +35,18 @@ const ClientResult = () => {
   });
 
   const handleDisplayStudentResult = (e) => {
-    e.preventDefault();
-        const form = e.target;
-        const session = form.session.value;
-        const examName = form.examName.value;
-        const className = form.className.value;
-        const classRoll = form.classRoll.value;
-        setSession(session);
-        setExamName(examName);
-        setClassName(className);
-        setClassRoll(classRoll);
-        refetch();
-    setEnabled(true);
     refetch();
+    e.preventDefault();
+    const form = e.target;
+    const session = form.session.value;
+    const examName = form.examName.value;
+    const className = form.className.value;
+    const classRoll = form.classRoll.value;
+    setSession(session);
+    setExamName(examName);
+    setClassName(className);
+    setClassRoll(classRoll);
+    setEnabled(true);
   };
   return (
     <>
@@ -55,9 +55,10 @@ const ClientResult = () => {
       </Helmet>
       <div className="bg-blue-50">
         <div className="card-body max-sm:p-3 lg:w-11/12 md:mx-auto">
-          <form 
-          onSubmit={handleDisplayStudentResult}
-          className="flex max-sm:flex-wrap md:justify-center md:items-center max-sm:gap-y-2 max-sm:gap-x-4 md:gap-5">
+          <form
+            onSubmit={handleDisplayStudentResult}
+            className="flex max-sm:flex-wrap md:justify-center md:items-center max-sm:gap-y-2 max-sm:gap-x-4 md:gap-5"
+          >
             {/* select year */}
             <div className="form-control flex-col justify-between md:items-start ">
               <label className="block max-sm:text-sm w-full label font-semibold">
@@ -103,7 +104,9 @@ const ClientResult = () => {
             </div>
             {/* class name */}
             <div className="form-control justify-between md:items-start ">
-              <label className="block max-sm:text-sm w-full label font-semibold">শ্রেণী :</label>
+              <label className="block max-sm:text-sm w-full label font-semibold">
+                শ্রেণী :
+              </label>
               <select
                 name="className"
                 className="select select-bordered"
@@ -134,7 +137,9 @@ const ClientResult = () => {
             </div>
             {/* class roll */}
             <div className="form-control justify-between md:items-start ">
-              <label className="block max-sm:text-sm  w-full label font-semibold">রোল নং :</label>
+              <label className="block max-sm:text-sm  w-full label font-semibold">
+                রোল নং :
+              </label>
               <input
                 type="number"
                 name="classRoll"
@@ -147,7 +152,7 @@ const ClientResult = () => {
             </div>
             <div className="form-control mt-auto">
               <button
-              type="submit"
+                type="submit"
                 className="btn-sm max-sm:rounded-md py-1 md:btn max-sm:btn-sm bg-green-700  hover:bg-green-600 text-white"
               >
                 সার্চ করুন
@@ -158,104 +163,120 @@ const ClientResult = () => {
         <div className="w-[95%] md:w-11/12 mx-auto pb-5">
           <div className={`max-sm:mx-2 md:w-11/12 mx-auto`}>
             {isLoading && <Loading />}
-            {resultData?.studentName && !isLoading ? (
-              <div
-                style={{ backgroundColor: "#bbf7d0" }}
-                className=" px-3 rounded-lg py-5 md:py-8"
-              >
-                <div className="flex flex-col justify-center items-center">
-                  <h2
-                    style={{ color: "#052e16" }}
-                    className="text-2xl md:text-4xl font-bold text-center"
-                  >
-                    শাহ্ নেয়ামত (রহঃ) কেজি এন্ড হাই স্কুল
-                  </h2>
-                  <h3 className="text-lg md:text-xl text-center font-semibold">
-                    {resultData?.examName} Exam: {resultData.session}
-                  </h3>
-                </div>
-                <div className="divider my-0"></div>
-                <div className="py-5 grid grid-cols-12 gap-y-2 justify-between p-5">
-                  <div className="col-span-12 md:col-span-7 max-sm:space-y-2 ">
-                    {/* student name */}
-                    <h3 className="text-md md:text-lg grid grid-cols-12  gap-1">
-                      <strong className="col-span-6 md:col-span-3">
-                        শিক্ষার্থীর নাম
-                      </strong>{" "}
-                      <span className="col-span-1">:</span>{" "}
-                      <span className="col-span-5 md:col-span-8">
-                        {resultData?.studentName}
-                      </span>
-                    </h3>
-                    {/* class name */}
-                    <h3 className="text-md md:text-lg grid grid-cols-12 gap-1">
-                      <strong className="col-span-6 md:col-span-3">
-                        শ্রেণী
-                      </strong>{" "}
-                      <span className="col-span-1">:</span>{" "}
-                      <span className="col-span-5 md:col-span-8">
-                        {resultData?.className}
-                      </span>
-                    </h3>
-                    {/* Roll no */}
-                    <h3 className="text-md md:text-lg grid grid-cols-12 gap-1">
-                      <strong className="col-span-6 md:col-span-3">রোল</strong>{" "}
-                      <span className="col-span-1">:</span>{" "}
-                      <span className="col-span-5 md:col-span-8">
-                        {resultData?.classRoll}
-                      </span>
+            <AnimatePresence>
+              {resultData?.studentName && !isLoading && (
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 50 }}
+                  transition={{ duration: 0.5 }}
+                  style={{ backgroundColor: "#bbf7d0" }}
+                  className=" px-3 rounded-lg py-5 md:py-8"
+                >
+                  <div className="flex flex-col justify-center items-center">
+                    <h2
+                      style={{ color: "#052e16" }}
+                      className="text-2xl md:text-4xl font-bold text-center"
+                    >
+                      শাহ্ নেয়ামত (রহঃ) কেজি এন্ড হাই স্কুল
+                    </h2>
+                    <h3 className="text-lg md:text-xl text-center font-semibold">
+                      {resultData?.examName} Exam: {resultData.session}
                     </h3>
                   </div>
-                  <div className=" md:ms-auto col-span-12 md:col-span-5">
-                    <h3 className="text-md md:text-lg grid grid-cols-12  gap-1">
-                      <strong className="col-span-6 md:col-span-7">
-                        মোট প্রাপ্ত নম্বর
-                      </strong>{" "}
-                      <span className="col-span-1">:</span>{" "}
-                      <span className="col-span-5 md:col-span-4">
-                        {resultData?.totalMarks}
-                      </span>
-                    </h3>
-                    <h3 className="text-md md:text-lg grid grid-cols-12  gap-1">
-                      <strong className="col-span-6 md:col-span-7">GPA</strong>{" "}
-                      <span className="col-span-1">:</span>{" "}
-                      <span className="col-span-5 md:col-span-4">
-                        {resultData?.totalGPA < 1
-                          ? 0
-                          : resultData?.totalGPA?.toFixed(2)}
-                      </span>
-                    </h3>
-                    <h3 className="text-md md:text-lg grid grid-cols-12 gap-1">
-                      <strong className="col-span-6 md:col-span-7 ">
-                        Letter Grade
-                      </strong>{" "}
-                      <span className="col-span-1">:</span>{" "}
-                      <span className="col-span-5 md:col-span-4">
-                        {resultData?.totalLG}
-                      </span>
-                    </h3>
-                    {/* Status */}
-                    <h3 className="text-md md:text-lg grid grid-cols-12 gap-1">
-                      <strong className="col-span-6 md:col-span-7">
-                        Status
-                      </strong>{" "}
-                      <span className="col-span-1">:</span>{" "}
-                      <span className="col-span-5 md:col-span-4">
-                        {resultData.status === "Pass" ? "পাস" : resultData.status === "Fail" ? "ফেইল" : "N/A"}
-                      </span>
-                    </h3>
+                  <div className="divider my-0"></div>
+                  <div className="py-5 grid grid-cols-12 gap-y-2 justify-between p-5">
+                    <div className="col-span-12 md:col-span-7 max-sm:space-y-2 ">
+                      {/* student name */}
+                      <h3 className="text-md md:text-lg grid grid-cols-12  gap-1">
+                        <strong className="col-span-6 md:col-span-3">
+                          শিক্ষার্থীর নাম
+                        </strong>{" "}
+                        <span className="col-span-1">:</span>{" "}
+                        <span className="col-span-5 md:col-span-8">
+                          {resultData?.studentName}
+                        </span>
+                      </h3>
+                      {/* class name */}
+                      <h3 className="text-md md:text-lg grid grid-cols-12 gap-1">
+                        <strong className="col-span-6 md:col-span-3">
+                          শ্রেণী
+                        </strong>{" "}
+                        <span className="col-span-1">:</span>{" "}
+                        <span className="col-span-5 md:col-span-8">
+                          {resultData?.className}
+                        </span>
+                      </h3>
+                      {/* Roll no */}
+                      <h3 className="text-md md:text-lg grid grid-cols-12 gap-1">
+                        <strong className="col-span-6 md:col-span-3">
+                          রোল
+                        </strong>{" "}
+                        <span className="col-span-1">:</span>{" "}
+                        <span className="col-span-5 md:col-span-8">
+                          {resultData?.classRoll}
+                        </span>
+                      </h3>
+                    </div>
+                    <div className=" md:ms-auto col-span-12 md:col-span-5">
+                      <h3 className="text-md md:text-lg grid grid-cols-12  gap-1">
+                        <strong className="col-span-6 md:col-span-7">
+                          মোট প্রাপ্ত নম্বর
+                        </strong>{" "}
+                        <span className="col-span-1">:</span>{" "}
+                        <span className="col-span-5 md:col-span-4">
+                          {resultData?.totalMarks}
+                        </span>
+                      </h3>
+                      <h3 className="text-md md:text-lg grid grid-cols-12  gap-1">
+                        <strong className="col-span-6 md:col-span-7">
+                          GPA
+                        </strong>{" "}
+                        <span className="col-span-1">:</span>{" "}
+                        <span className="col-span-5 md:col-span-4">
+                          {resultData?.totalGPA < 1
+                            ? 0
+                            : resultData?.totalGPA?.toFixed(2)}
+                        </span>
+                      </h3>
+                      <h3 className="text-md md:text-lg grid grid-cols-12 gap-1">
+                        <strong className="col-span-6 md:col-span-7 ">
+                          Letter Grade
+                        </strong>{" "}
+                        <span className="col-span-1">:</span>{" "}
+                        <span className="col-span-5 md:col-span-4">
+                          {resultData?.totalLG}
+                        </span>
+                      </h3>
+                      {/* Status */}
+                      <h3 className="text-md md:text-lg grid grid-cols-12 gap-1">
+                        <strong className="col-span-6 md:col-span-7">
+                          Status
+                        </strong>{" "}
+                        <span className="col-span-1">:</span>{" "}
+                        <span className="col-span-5 md:col-span-4">
+                          {resultData.status === "Pass"
+                            ? "পাস"
+                            : resultData.status === "Fail"
+                            ? "ফেইল"
+                            : "N/A"}
+                        </span>
+                      </h3>
+                    </div>
                   </div>
-                </div>
-                
-              </div>
-            ) : serverError ? (
-              <p className="text-red-500 max-sm:text-xs text-center">{serverError}</p>
-            ) :  (
-              <p className="max-sm:text-xs text-center">
-                দয়া করে শিক্ষাবর্ষ, পরীক্ষা, শ্রেণী ও রোল টাইপ করুন এবং ফলাফল দেখতে
-                সার্চ এ ক্লিক করুন...
+                </motion.div>
+              )}
+            </AnimatePresence>
+            {serverError ? (
+              <p className="text-red-500 max-sm:text-xs text-center">
+                {serverError}
               </p>
-            )}
+            ) : !resultData?.studentName && !isLoading ? (
+              <p className="max-sm:text-xs text-center">
+                দয়া করে শিক্ষাবর্ষ, পরীক্ষা, শ্রেণী ও রোল টাইপ করুন এবং ফলাফল
+                দেখতে সার্চ এ ক্লিক করুন...
+              </p>
+            ) : null}
           </div>
         </div>
       </div>
